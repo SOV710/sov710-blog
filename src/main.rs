fecae4d9f0c726264342f8313065087bf771b5b1
SOV710 <chris916911179@outlook.com>
2026-02-04 12:22:31 +0800

    feat: add main structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+pub mod ast;
+pub mod html;
+
+use orgize::Org;
+use std::fs;
+
+use crate::ast::AstExporter;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let org_content = fs::read_to_string("src/test.org")
+        .map_err(|e| format!("FAILED to READ test.org: {}", e))?;
+
+    let org = Org::parse(org_content);
+    let mut exporter = AstExporter::default();
+    org.traverse(&mut exporter);
+
+    println!("{}", exporter.output);
+    Ok(())
 }
